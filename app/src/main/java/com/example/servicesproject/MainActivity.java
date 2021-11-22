60758b6c02c5c9d8bbcbed24a1052c8d4a7a4e8e
mohammad <m.zandieh1999@gmail.com>
2021-11-22 12:18:18 +0330

    broadcast receiver

--- app/src/main/java/com/example/servicesproject/MainActivity.java
+++ app/src/main/java/com/example/servicesproject/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.servicesproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonStartService = findViewById(R.id.buttonStart);
         Button buttonStopService = findViewById(R.id.buttonStop);
-
+        TextView textViewCounter = findViewById(R.id.textViewCounter);
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {                                 //when send each message , this method is called.
+                Toast.makeText(context, "done!", Toast.LENGTH_SHORT).show();
+            }
+        };
         buttonStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,MyService.class);
-//                startService(intent);
-
-                Intent intent = new Intent(MainActivity.this,MyIntentService.class);
+                Intent intent = new Intent(MainActivity.this, MyIntentService.class);
+                intent.putExtra("key", "sendEventFromActivity");
                 startService(intent);
             }
         });
@@ -32,9 +44,23 @@ public class MainActivity extends AppCompatActivity {
         buttonStopService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,MyService.class);
-//                stopService(intent);
+
             }
         });
+    }
+
+//register
+    @Override
+    protected void onResume() {                                                                     //last method that run and activity launch. so user can see activity after this method.
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter("com.example.action.service");
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+//unRegister
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
     }
 }
--- app/src/main/java/com/example/servicesproject/MyIntentService.java
+++ app/src/main/java/com/example/servicesproject/MyIntentService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class MyIntentService extends IntentService {
 
@@ -20,16 +21,23 @@ public class MyIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {                                        //when we call this service , this method is called
+        String username = intent.getExtras().getString("key");
+
         Log.d("service", "onHandleIntent: " + Thread.currentThread().getName());
+
         for (int i = 0; i < 5; i++) {
             try {
                 Thread.sleep(1000);
                 Log.d("service", "work: #" + i + " " + Thread.currentThread().getName());
+                
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-    }
+        }                                                                                           //this block : get and put data from service.
+        Intent localIntent = new Intent("com.example.action.service");
+        localIntent.putExtra("key", true);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);                         //LocalBroadcastManager: send message to all local component in my app
+    }                                                                                               // 3 khate bala : jaavbe service baraye view moon.
 
     @Override
     public void onDestroy() {
